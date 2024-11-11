c7dc51d64cdbc6ba8bf7fbc0194799bb3c27d82a
amilmohd155 <32008354+amilmohd155@users.noreply.github.com>
2024-11-11 16:45:40 +0000

    test

--- lib/cdk-example-stack.ts
+++ lib/cdk-example-stack.ts
@@ -1,16 +1,62 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import {
+  ManagedPolicy,
+  PolicyStatement,
+  Role,
+  ServicePrincipal,
+} from "aws-cdk-lib/aws-iam";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkExampleStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const table = new dynamodb.TableV2(this, "UrlShortenerTable", {
+      tableName: "UrlShortenerTable",
+      partitionKey: {
+        name: "pk",
+        type: dynamodb.AttributeType.STRING,
+      },
+      sortKey: {
+        name: "sk",
+        type: dynamodb.AttributeType.STRING,
+      },
+      timeToLiveAttribute: "expires",
+      billing: dynamodb.Billing.onDemand(),
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // NOT recommended for production code
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkExampleQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    table.addGlobalSecondaryIndex({
+      indexName: "GST1",
+      partitionKey: {
+        name: "GSI1PK",
+        type: dynamodb.AttributeType.STRING,
+      },
+      sortKey: {
+        name: "GSI1SK",
+        type: dynamodb.AttributeType.STRING,
+      },
+    });
+
+    new Role(this, "DynamodbAccessRole", {
+      assumedBy: new ServicePrincipal("dynamodb.amazonaws.com"),
+      // managedPolicies: [
+      //   ManagedPolicy.fromAwsManagedPolicyName("AmazonDynamoDBFullAccess"),
+      // ],
+    }).addToPolicy(
+      new PolicyStatement({
+        actions: [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:UpdateItem",
+          "dynamodb:DeleteItem",
+          "dynamodb:Query",
+          "dynamodb:Scan",
+        ],
+        resources: [table.tableArn],
+      })
+    );
   }
 }
